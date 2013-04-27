8d72c0bd72bb5f37bc25e90a6846a3e089095d48
ishenli <meshenli@gmail.com>
2013-04-27 22:30:30 +0800

    添加回调

--- js/application.js
+++ js/application.js
@@ -1,6 +1,5 @@
 /**
  * Created with JetBrains WebStorm.
- * User: SH201
  * Date: 13-4-19
  * Time: 下午4:33
  * 添加常用的函数
@@ -17,4 +16,19 @@
           alert("login");
       });
     })
+})(window, jQuery);
+/**
+ * qq登陆
+ * @param window
+ * @param $
+ */
+(function(w, $) {
+    $("#qq_content_btn").on("click",function(){
+        QC.Login.showPopup({
+            appId:"100370679",
+            redirectURI:"http://marrymemo.com/marry/qc_back.html"
+        })
+    })
+    //调用QC.Login方法，指定btnId参数将按钮绑定在容器节点中
+
 })(window, jQuery);
