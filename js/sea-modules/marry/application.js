c7eab3fcaa07e2714304d70ff802c3f7861732c3
ishenli <meshenli@gmail.com>
2013-04-29 13:38:56 +0800

    去掉多余的js文件

--- js/sea-modules/marry/application.js
+++ js/sea-modules/marry/application.js
@@ -9,6 +9,7 @@
 define(function(require, exports, module) {
     var $=require("marry/extendJq"),
         util=require("marry/util"),
+        Base = require('arale/base/1.0.1/base');
         Widget = require('arale/widget/1.0.3/widget');
 
     (function(window){
@@ -35,33 +36,66 @@ define(function(require, exports, module) {
     }(window))
 
     var App={};
-     App.Note=Widget.extend({
-         events: {
-             'click .close': 'remove'
-         },
-         /*initialize:function(){
-           this.drag();
-       },*/
-       drag:function(){
-           $("#addList").dragsort({
-               dragSelector: "article", dragBetween: true, dragEnd:this.afterDrag(),
-               placeHolderTemplate: "<article class='placeHolder'>放于该位置</article>"
-           });
-       },
-       afterDrag:function(number){
-           /*var number=1;
-           $("#addList article").each(function(){
-               $(this).find(".number").text(number);
-               number++;
-           });*/
-       },
-       remove:function(event){
-           event.preventDefault();
-           $(event.target).parent().remove();
-           adjustFootPos();
-       }
+    App.Note=Widget.extend({
+        events: {
+            'click .close': 'remove'
+        },
+        /*initialize:function(){
+         this.drag();
+         },*/
+        drag:function(){
+            $("#addList").dragsort({
+                dragSelector: "article", dragBetween: true, dragEnd:this.afterDrag(),
+                placeHolderTemplate: "<article class='placeHolder'>放于该位置</article>"
+            });
+        },
+        afterDrag:function(number){
+            /*var number=1;
+             $("#addList article").each(function(){
+             $(this).find(".number").text(number);
+             number++;
+             });*/
+        },
+        remove:function(event){
+            event.preventDefault();
+            $(event.target).parent().remove();
+            adjustFootPos();
+        }
     });
-
+    App.Comment=Base.extend({
+        get:function(id,element){
+            util.FlyJSONP.get({
+                url:'http://marrymemo.com:3000/montages/'+id+'/discussions.json',
+                success:function(result){
+                var data=result.discussions;
+                    var htmlTep='<li class="big-avatar"> <img src="{pic}" class="fn-left"> <div class="comments-text"> <h3>{name}</h3> <p>{content}</p> <span class="date">{date}</span> </div> </li>';
+                    var output='';
+                    for(var i in data){
+                       output+=htmlTep.replace("{pic}",'http://marrymemo.com:3000/'+data[i].user.avatar).replace("{name}",data[i].user.nick).replace("{content}",data[i].content)
+                           .replace("{date}",data[i].created_at.substring(0,10));
+                   }
+                    $(element).append(output);
+                }
+            });
+        },
+        post:function(id,userid,input,callback){
+            util.FlyJSONP.post({
+            url: 'http://marrymemo.com:3000/montages/'+id+'/discussions.json',
+            parameters: {
+                "discussion": {
+                    "content" :$(input).val(),
+                    "user_id" :userid
+                }
+            },
+            error:function(){
+                alert("error");
+            },
+            success: function(data) {
+                alert(data);
+            }
+            })
+        }
+    });
     module.exports = App;
 });
 
