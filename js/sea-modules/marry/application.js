c824189f3ad85790da7b54386836fc91e7c04351
ishenli <meshenli@gmail.com>
2013-06-05 23:51:09 +0800

    完善请帖制作

--- js/sea-modules/marry/application.js
+++ js/sea-modules/marry/application.js
@@ -357,15 +357,17 @@ define(function(require, exports, module) {
             util.FlyJSONP.get({
                 url:HOST + 'themes.json',
                 success:function(data){
-                    var html="",template='<li><a href="javascript:void(0)"> <img src="{pic}" alt="{name}"/> </a></li>';
+                    var html="",template='<li data-id={id}><a href="javascript:void(0)"> <img src="{pic}" alt="{name}"/> </a></li>';
 //                    var len=(data.length<4?data.length:4)
                     var len=data.length;
                     for(var i=0;i<len;i++){
-                        html+=template.replace("{pic}",HOST+data[i].thumb_path).replace("{name}",data[i].name);
+                        html+=template.replace("{pic}",HOST+data[i].thumb_path).replace("{name}",data[i].name)
+                            .replace("{id}",data[i].id);
                     }
                     $(option.element).html(html);
                 }
             })
+            option.callback();
         }
     })
 module.exports = App;
