6130233a733e6e228b44444079ec796fa2cae306
ishenli <meshenli@gmail.com>
2013-06-19 16:44:31 +0800

    更新seajs模块配置

--- js/config.js
+++ js/config.js
@@ -2,13 +2,14 @@
  * Created with JetBrains WebStorm.
  * User: shenli
  * Date: 13-6-19
- * Time: 下午4:23
  * seajs config
  **/
  seajs.config({
     alias: {
         "$": "gallery/jquery.js",
         "dialog":"arale/dialog/1.1.1/dialog",
-        "tip":"arale/tip/1.1.3/tip"
+        "tip":"arale/tip/1.1.3/tip",
+        "carousel":"arale/switchable/0.9.14/carousel",
+        "validator":"arale/validator/0.9.3/validator"
     }
 });
