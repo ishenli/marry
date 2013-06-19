c01e03235b1a6ce42ae71d77956ca165ac9d0b46
ishenli <meshenli@gmail.com>
2013-06-19 16:29:37 +0800

    添加seajs应用配置

---
+++ js/config.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: shenli
+ * Date: 13-6-19
+ * Time: 下午4:23
+ * seajs config
+ **/
+ seajs.config({
+    alias: {
+        "$": "gallery/jquery.js",
+        "dialog":"arale/dialog/1.1.1/dialog",
+        "tip":"arale/tip/1.1.3/tip"
+    }
+});
